c6ab48141f90af50c4dfe213217ac08137179711
LayneHansen <layne.d.hansen@gmail.com>
2020-10-05 19:32:59 -0600

    moved public folder outside develop folder, re-pathed files

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 const { response } = require("express");
 var express = require("express");
-
 var app = express();
-
 var path = require("path");
-
 var PORT = process.env.PORT || 3003;
-
-// var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 // app.use(express.static(__dirname, "/public"));
+// app.use("/js", express.static(__dirname + "public/js"));
+// app.use("/css", express.static(__dirname + "public/css"));
 
 // require("./routes/apiRoutes");
 // require("./routes/htmlRoutes");
 
+app.get("/", function (request, response) {
+    response.sendFile(path.join(__dirname, "/public"));
+});
+
 app.get("*", function (request, response) {
-    response.sendFile(path.join(__dirname + "/Develop/public/index.html"));
+    response.sendFile(path.join(__dirname + "/public/index.html"));
 })
 
 app.get("/notes", function (request, response) {
-    response.sendFile(path.join(__dirname + "/Develop/public/notes.html"));
+    response.sendFile(path.join(__dirname + "/public/notes.html"));
 })
 
-app.get("/notes", function (request, response) {
-    response.sendFile(path.join(__dirname + "/Develop/public/assets/css/styles.css"));
+app.get("/index", function (request, response) {
+    response.sendFile(path.join(__dirname + "/public/assets/css/styles.css"));
 })
 
 
-// app.get("/", function (request, response) {
-//     response.sendFile(path.join(__dirname, "/Develop/public"));
-// });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
